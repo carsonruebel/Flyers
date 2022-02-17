27c161b0e4351f8221f6fb0da1491fdce76fabcd
Carson Ruebel <cruebel@outlook.com>
2022-02-17 11:22:05 -0800

    escaping single quotes pushed from xlsx to python

--- v2 ETL/DailyLoadFlyers.py
+++ v2 ETL/DailyLoadFlyers.py
@@ -17,11 +17,14 @@ def pull_data(storename):
     df_new = df_new.drop(['score','item_weight','bottom','left','right','top'], axis = 1)
     df_new = df_new.dropna(subset=['current_price'])
     df_new.insert(0, 'Industry', 'Supermarket')
+    df_new['name'] = df_new['name'].str.replace("'","''")
     #df_new.rename(columns={"name": "Product", "merchant_name": "Merchant"}) #placeholder in case I want to rename columns
     #print(df_new)
     #Only uncomment below to start a new file
     if exists('adhistory.xlsx') == 0:
-        df_new.to_excel('adhistory.xlsx', index=False)
+        #df_new.to_excel('adhistory.xlsx', index=False)
+        with pd.ExcelWriter('adhistory.xlsx') as writer:  
+            df_new.to_excel(writer, sheet_name='RawInput', index=False)
     #if exists('adhistory2.json') == 0:
     #    df_new.to_json('adhistory2.json')
     return df_new
@@ -44,7 +47,9 @@ def merge_data(filename, dataframe):
     added_row_count = df_final.shape[0] - df_old.shape[0]
 
     #store back as original file
-    df_final.to_excel(filename + ".xlsx", index=False)
+    #df_final.to_excel(filename + ".xlsx", index=False)
+    with pd.ExcelWriter('adhistory.xlsx', mode='a', if_sheet_exists='replace') as writer:  
+        df_final.to_excel(writer, sheet_name='RawInput', index=False)
     #df_final.to_json(filename + ".json")
     return added_row_count
 
