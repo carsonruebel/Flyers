66ef15bc2e9697fb9c3f11808764666b21c33da6
cruebel <cruebel@outlook.com>
2023-11-11 06:58:37 +0800

    Updated URI address to new subscription & cleaned up SQL queries for medium post

--- v1 ETL/Flyers SQL Queries.sql
+++ v1 ETL/Flyers SQL Queries.sql
@@ -1,3 +1,8 @@
+/*
+	6 queries to create 6 tables
+*/
+
+
 CREATE TABLE DIM_Industry (
     IndustryID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
     Industry VARCHAR(50) NOT NULL,
@@ -44,6 +49,10 @@ CREATE TABLE FACT_Ad (
 
 ---------------
 
+/*
+	6 queries to drop (completely remove) 6 tables
+*/
+
 DROP TABLE DIM_Industry;
 DROP TABLE DIM_Merchant;
 DROP TABLE DIM_Brand;
@@ -53,14 +62,69 @@ DROP TABLE FACT_Ad;
 
 ---------------
 
+/*
+	Creates stored procedure used to insert data into dim & fact tables
+*/
 
+CREATE PROCEDURE InsertNormalizedAd
+	@Industry VARCHAR(50),
+	@Merchant VARCHAR(100),
+	@Brand VARCHAR(100),
+	@Product VARCHAR(100),
+	@ProductDescription VARCHAR(255),
+	@Category VARCHAR(100),
+	@GeneralProduct VARCHAR(100),
+	@URI VARCHAR(255),
+	@StartDate DATE,
+	@EndDate DATE,
+	@Distinction VARCHAR(50),
+	@Amount VARCHAR(50),
+	@MinQuantity INT,
+	@MaxQuantity INT,
+	@Price DECIMAL(10,2)
+AS   
+	INSERT INTO DIM_Industry (Industry) VALUES (@Industry);
+	INSERT INTO DIM_Merchant (Merchant) VALUES (@Merchant);
+	INSERT INTO DIM_Brand (Brand) VALUES (@Brand);
+	INSERT INTO DIM_Product (Product, ProductDescription, Category, GeneralProduct) VALUES (@Product, @ProductDescription, @Category, @GeneralProduct);
+	INSERT INTO DIM_Flyer (URI) VALUES (@URI);
+	INSERT INTO FACT_Ad (StartDate, EndDate, IndustryID, MerchantID, BrandID, ProductID, FlyerID, Distinction, Amount, MinQuantity, MaxQuantity, Price)
+		VALUES (
+			@StartDate, @EndDate,
+			(SELECT MAX(IndustryID) from DIM_Industry),
+			(SELECT MAX(MerchantID) FROM DIM_Merchant),
+			(SELECT MAX(BrandID) FROM DIM_Brand),
+			(SELECT MAX(ProductID) FROM DIM_Product),
+			(SELECT MAX(FlyerID) FROM DIM_Flyer),
+			@Distinction, @Amount, @MinQuantity, @MaxQuantity, @Price); 
+GO  
+
+---------------
+
+/*
+	creates stored procedure to clear all data from 6 tables
+	Also contains exec command to execute the stored proc
+*/
+
+CREATE PROCEDURE EmptyAllTables
+AS
+	DELETE FROM FACT_Ad;
+	DELETE FROM DIM_Industry;
+	DELETE FROM DIM_Merchant;
+	DELETE FROM DIM_Brand;
+	DELETE FROM DIM_Product;
+	DELETE FROM DIM_Flyer;
+GO
 
 
 exec EmptyAllTables
 
+---------------
 
+/*
+	Query to convert the data stored in dim & fact tables back to spreadsheet format from manual data entry
+	*/
 
---Unnormalize data back to entry spreadsheet
 SELECT i.Industry, m.Merchant, b.Brand, p.Product, p.ProductDescription, p.Category, p.GeneralProduct, f.URI, fact.StartDate, fact.EndDate, fact.Distinction, fact.Amount, fact.MinQuantity, fact.MaxQuantity, fact.Price
 FROM [dbo].[FACT_Ad] fact
 LEFT JOIN DIM_Industry i on fact.IndustryID = i.IndustryID
@@ -68,5 +132,3 @@ LEFT JOIN DIM_Merchant m on fact.MerchantID = m.MerchantID
 LEFT JOIN DIM_Brand b on fact.BrandID = b.BrandID
 LEFT JOIN DIM_Product p on fact.ProductID = p.ProductID
 LEFT JOIN DIM_Flyer f on fact.FlyerID = f.FlyerID
-
-select top 5 * from FACT_Ad
